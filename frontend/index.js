f749d8043c547f72b712dada3b12aa88ed1ba0cf
lakshmi tanooja <tanoojaml@gmail.com>
2026-07-03 16:02:09 +0530

    Initiating Toggle

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,22 @@
+const users=[
+    {
+        name:"Jane Doe",
+        gender:"Female",
+        
+    },
+    {
+        name:"John Doe",
+        gender:"Male"
+    }
+]
+let currId=0;
+function toggle(){
+    if(currId==0){
+        currId=1;
+    }
+    else{
+        currId=0;
+    }
+}
+
 console.log("Hello from JS");
-alert("Hello from JS");
