392561e3c3c41ed734add57cc17eece49dbd992a
lakshmi tanooja <tanoojaml@gmail.com>
2026-07-03 16:07:39 +0530

    Initiating toggle with errors

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,13 @@
 const users=[
     {
-        name:"Jane Doe",
-        gender:"Female",
-        
+        "name":"Jane Doe",
+        "gender":"Female",
+        "image":"./images/jane.png"
     },
     {
-        name:"John Doe",
-        gender:"Male"
+        "name":"John Doe",
+        "gender":"Male",
+        "image":"./images/john.png"
     }
 ]
 let currId=0;
@@ -17,6 +18,8 @@ function toggle(){
     else{
         currId=0;
     }
+document.getElementById("name").innerHTML=users[currId].name;
+document.getElementById("gender").innerHTML=users[currId].gender;
+document.getElementById("image").src=users[currId].image;
 }
-
 console.log("Hello from JS");
